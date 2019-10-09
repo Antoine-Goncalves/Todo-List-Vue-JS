16f9c78fc7bad87be1f8564a8e0a4e4bba9f6a33
Antoine-Goncalves <goncalves.devweb@gmail.com>
2019-10-09 19:31:15 +0200

    add data to component TodoList.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -7,6 +7,11 @@ import TodoInputText from "@/components/TodoInputText.vue";
 export default {
   components: {
     TodoInputText
+  },
+  data() {
+    return {
+      todos: []
+    };
   }
 };
 </script>
