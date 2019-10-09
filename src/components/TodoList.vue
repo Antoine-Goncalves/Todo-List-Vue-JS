07480c3be7013d72f0eb9a1d5cf15da54ee0a3cd
Antoine-Goncalves <goncalves.devweb@gmail.com>
2019-10-09 19:38:41 +0200

    link component TodoListItem.vue to TodoList.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,18 @@
 <template>
-  <TodoInputText />
+  <div>
+    <TodoInputText />
+    <TodoListItem :todos="todos" />
+  </div>
 </template>
 
 <script>
 import TodoInputText from "@/components/TodoInputText.vue";
+import TodoListItem from "@/components/TodoListItem.vue";
+
 export default {
   components: {
-    TodoInputText
+    TodoInputText,
+    TodoListItem
   },
   data() {
     return {
